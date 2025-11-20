ab897a12e279de9569fe836132c49ede2c82df7b
Erick-Messias-dev <erick.messias@outolook.com.br>
2025-11-20 12:03:42 -0300

    pequenas mudaças

--- FilmApi/Domain/Models/Session.cs
+++ FilmApi/Domain/Models/Session.cs
@@ -7,4 +7,7 @@ public class Session
     [Key]
     [Required]  
     public int Id { get; set; }
+    [Required]
+    public int FilmId { get; set; }
+    public virtual Film Film { get; set; }
 }
--- FilmApi/Profiles/SessionProfile.cs
+++ FilmApi/Profiles/SessionProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace FilmApi.Profiles
+﻿using AutoMapper;
+using FilmApi.Data.Dto;
+using FilmApi.Domain.Models;
+
+namespace FilmApi.Profiles;
+
+public class SessionProfile : Profile
 {
-    public class SessionProfile
+    public SessionProfile()
     {
+        CreateMap<CreateSessionDto, Session>();
+        CreateMap<Session, ReadSessionDto>();
+
     }
 }
